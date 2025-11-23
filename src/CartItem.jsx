0387298a204981dc6221aab316969ee8f4af775f
Charles Huang <cckhuang@users.noreply.github.com>
2025-11-23 18:55:40 -0500

    Calculate shopping cart total

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,13 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+    cart.forEach(item => {
+      const quantity = item.quantity;
+      const cost = item.cost;
+      total += parseFloat(cost.substring(1)) * quantity;
+    });
+    return total;
   };
 
   const handleContinueShopping = (e) => {
